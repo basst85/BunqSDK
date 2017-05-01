aa2b6821d39f20d7aae83eebee502f5045551414
JvanHengel <jellevhengel@gmail.com>
2017-05-01 21:46:58 +0200

    Added post method

--- src/Bunq/Setup/SessionServer.php
+++ src/Bunq/Setup/SessionServer.php
@@ -56,11 +56,77 @@ class SessionServer
     public function __construct($secret, $httpClient = null)
     {
         $this->secret = $secret;
-        $this->httpClient = httpClient ?: new BunqClient();
+        $this->httpClient = $httpClient ?: new BunqClient();
     }
 
-    public function post()
+    /**
+     * Posts a SessionServerRequest to the server.
+     * Saves the server response in the sessionServerResponse field.
+     *
+     * @param $clientPrivateKey String The client private key used for signing the request.
+     * @param $installationToken String The installation token from the installation request.
+     * @param $serverPublicKey String The server public key used for verification.
+     * @param $customRequestHeaders null|array the custom headers for the request.
+     * if $customRequestHeaders is null, the default headers will be used.
+     *
+     * @throws BunqObjectException thrown if the required attributes are missing.
+     * @throws BunqVerificationException thrown if the response verification fails.
+     */
+    public function post($clientPrivateKey, $installationToken, $serverPublicKey, $customRequestHeaders = null)
     {
+        //Create the requestHeaders.
+        $requestHeaders = $customRequestHeaders ?: [
+            self::HEADER_REQUEST_CACHE_CONTROL => 'no-cache',
+            self::HEADER_REQUEST_USER_AGENT => 'SandboxPublicApi:DefaultUser',
+            self::HEADER_REQUEST_CUSTOM_AUTHENTICATION => $installationToken,
+            self::HEADER_REQUEST_CUSTOM_REQUEST_ID => $this->createUuid(),
+            self::HEADER_REQUEST_CUSTOM_GEOLOCATION => '0 0 0 0 000',
+            self::HEADER_REQUEST_CUSTOM_LANGUAGE => 'en_US',
+            self::HEADER_REQUEST_CUSTOM_REGION => 'en_US',
+        ];
 
+        //Create the requestBody.
+        if(is_null($this->secret)) {
+            throw new BunqObjectException('Missing required attributes.');
+        }
+
+        $arrayBody = ['secret' => $this->secret];
+
+        $requestBody = json_encode($arrayBody);
+
+        //Create the requestEndpoint and requestMethod.
+        $requestEndpoint = 'session-server';
+        $requestMethod = 'POST';
+
+        //Create the request which will be send to the server.
+        $sessionServerRequest = new BunqRequest($requestEndpoint, $requestMethod, $requestBody, $requestHeaders);
+
+        //Sign the request with the installation private key.
+        $signature = $this->httpClient->getRequestSignature($sessionServerRequest, $clientPrivateKey);
+
+        //Add the request signature to the headers.
+        $sessionServerRequest->setHeader(self::HEADER_REQUEST_CUSTOM_SIGNATURE, $signature);
+
+        //Execute the installationRequest and store it in the installationResponse field.
+        $this->sessionServerResponse = $this->httpClient->SendRequest($sessionServerRequest);
+
+        //Verify the response.
+        if(!$this->httpClient->verifyResponseSignature($this->sessionServerResponse, $serverPublicKey)) {
+            throw new BunqVerificationException('Response verification failed.');
+        }
+    }
+
+    /**
+     * Create a new unique identifier.
+     *
+     * @return string The unique identifier.
+     */
+    private function createUuid()
+    {
+        $randomInput = openssl_random_pseudo_bytes(16);
+        $randomInput[6] = chr(ord($randomInput[6]) & 0x0f | 0x40);
+        $randomInput[8] = chr(ord($randomInput[8]) & 0x3f | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($randomInput), 4));
     }
 }
