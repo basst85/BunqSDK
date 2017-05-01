ac997984152a56dcf603fcf203590900a74d932a
JvanHengel <jellevhengel@gmail.com>
2017-05-01 21:18:08 +0200

    Added SessionServer class

---
+++ src/Bunq/Setup/SessionServer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bunq\Setup;
+
+include_once('Client/BunqClient.php');
+include_once('Client/BunqRequest.php');
+include_once('Client/BunqResponse.php');
+include_once('Exceptions/BunqObjectException.php');
+
+use Bunq\Client\BunqClient;
+use Bunq\Client\BunqRequest;
+use Bunq\Client\BunqResponse;
+use Bunq\Exceptions\BunqObjectException;
+use Bunq\Exceptions\BunqVerificationException;
+
+/**
+ * Class SessionServer
+ * Class for the session-server endpoint.
+ * @package Bunq\Setup
+ */
+class SessionServer
+{
+
+}
