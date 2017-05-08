7618bab1ef59eac7d2ac9d3f2e83e1151881d1f8
JvanHengel <jellevhengel@gmail.com>
2017-05-08 11:37:24 +0200

    Added method clauses

--- src/Bunq/Setup/DeviceServer.php
+++ src/Bunq/Setup/DeviceServer.php
@@ -2,24 +2,53 @@
 
 namespace Bunq\Setup;
 
-include_once('Client/BunqClient.php');
-include_once('Client/BunqRequest.php');
+include_once('BunqObject.php');
 include_once('Client/BunqResponse.php');
 include_once('Exceptions/BunqObjectException.php');
-include_once('Exceptions/BunqVerificationException.php');
 
-use Bunq\Client\BunqClient;
-use Bunq\Client\BunqRequest;
+use Bunq\BunqObject;
 use Bunq\Client\BunqResponse;
 use Bunq\Exceptions\BunqObjectException;
-use Bunq\Exceptions\BunqVerificationException;
+
 
 /**
  * Class DeviceServer
  * Class for the device-server endpoint.
  * @package Bunq\Setup
  */
-class DeviceServer
+class DeviceServer extends BunqObject
 {
 
+    /**
+     * @return array the request body as an array.
+     */
+    public function getRequestBodyArray()
+    {
+        // TODO: Implement getRequestBodyArray() method.
+    }
+
+    /**
+     * Extracts the response data and stores them in the class fields.
+     * @param BunqResponse $response the response returned by the server.
+     */
+    public function serializeData(BunqResponse $response)
+    {
+        // TODO: Implement serializeData() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndpoint()
+    {
+        // TODO: Implement getEndpoint() method.
+    }
+
+    /**
+     * @param mixed $endpoint
+     */
+    public function setEndpoint($endpoint)
+    {
+        // TODO: Implement setEndpoint() method.
+    }
 }
