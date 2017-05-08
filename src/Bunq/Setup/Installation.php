d04ad78bcf967170bb094294ca996949541ef184
JvanHengel <jellevhengel@gmail.com>
2017-05-08 11:15:07 +0200

    Implemented serializeData method

--- src/Bunq/Setup/Installation.php
+++ src/Bunq/Setup/Installation.php
@@ -5,6 +5,7 @@ namespace Bunq\Setup;
 include_once('BunqObject.php');
 
 use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
 
 /**
  * Class Installation
@@ -33,5 +34,17 @@ class Installation extends BunqObject
     public function getRequestBodyArray()
     {
         return ['client_public_key' => $this->clientPublicKey];
+    }
+
+    public function serializeData(BunqResponse $response)
+    {
+        $this->id = json_decode($response->getBodyString())->{'Response'}[0]->{'Id'};
+
+        $this->tokenId = json_decode($response->getBodyString())->{'Response'}[1]->{'Token'}->{'id'};
+        $this->tokenCreated = json_decode($response->getBodyString())->{'Response'}[1]->{'Token'}->{'created'};
+        $this->tokenUpdated = json_decode($response->getBodyString())->{'Response'}[1]->{'Token'}->{'updated'};
+        $this->token = json_decode($response->getBodyString())->{'Response'}[1]->{'Token'}->{'token'};
+
+        $this->serverPublicKey = json_decode($response->getBodyString())->{'Response'}[2]->{'ServerPublicKey'}->{'server_public_key'};
     }
 }
