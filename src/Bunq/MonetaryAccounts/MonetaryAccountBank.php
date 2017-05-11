ac0ec719dd3fcaf15ea82720f33fb0bb56c5e4de
JvanHengel <jellevhengel@gmail.com>
2017-05-11 11:45:27 +0200

    Added getters and setters

--- src/Bunq/MonetaryAccounts/MonetaryAccountBank.php
+++ src/Bunq/MonetaryAccounts/MonetaryAccountBank.php
@@ -46,4 +46,259 @@ class MonetaryAccountBank extends BunqObject
         $this->monetaryAccountBank = json_decode($response->getBodyString())->{'Response'}[0]->{'MonetaryAccountBank'};
     }
 
+    /**
+     * @return mixed
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param mixed $currency
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDailyLimitValue()
+    {
+        return $this->dailyLimitValue;
+    }
+
+    /**
+     * @param mixed $dailyLimitValue
+     */
+    public function setDailyLimitValue($dailyLimitValue)
+    {
+        $this->dailyLimitValue = $dailyLimitValue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDailyLimitCurrency()
+    {
+        return $this->dailyLimitCurrency;
+    }
+
+    /**
+     * @param mixed $dailyLimitCurrency
+     */
+    public function setDailyLimitCurrency($dailyLimitCurrency)
+    {
+        $this->dailyLimitCurrency = $dailyLimitCurrency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAvatarUuid()
+    {
+        return $this->avatarUuid;
+    }
+
+    /**
+     * @param mixed $avatarUuid
+     */
+    public function setAvatarUuid($avatarUuid)
+    {
+        $this->avatarUuid = $avatarUuid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubstatus()
+    {
+        return $this->substatus;
+    }
+
+    /**
+     * @param mixed $substatus
+     */
+    public function setSubstatus($substatus)
+    {
+        $this->substatus = $substatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * @param mixed $reason
+     */
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReasonDescription()
+    {
+        return $this->reasonDescription;
+    }
+
+    /**
+     * @param mixed $reasonDescription
+     */
+    public function setReasonDescription($reasonDescription)
+    {
+        $this->reasonDescription = $reasonDescription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotificationFiltersNotificationDeliveryMethod()
+    {
+        return $this->notificationFiltersNotificationDeliveryMethod;
+    }
+
+    /**
+     * @param mixed $notificationFiltersNotificationDeliveryMethod
+     */
+    public function setNotificationFiltersNotificationDeliveryMethod($notificationFiltersNotificationDeliveryMethod)
+    {
+        $this->notificationFiltersNotificationDeliveryMethod = $notificationFiltersNotificationDeliveryMethod;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotificationFiltersNotificationTarget()
+    {
+        return $this->notificationFiltersNotificationTarget;
+    }
+
+    /**
+     * @param mixed $notificationFiltersNotificationTarget
+     */
+    public function setNotificationFiltersNotificationTarget($notificationFiltersNotificationTarget)
+    {
+        $this->notificationFiltersNotificationTarget = $notificationFiltersNotificationTarget;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotificationFiltersCategory()
+    {
+        return $this->notificationFiltersCategory;
+    }
+
+    /**
+     * @param mixed $notificationFiltersCategory
+     */
+    public function setNotificationFiltersCategory($notificationFiltersCategory)
+    {
+        $this->notificationFiltersCategory = $notificationFiltersCategory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSettingColor()
+    {
+        return $this->settingColor;
+    }
+
+    /**
+     * @param mixed $settingColor
+     */
+    public function setSettingColor($settingColor)
+    {
+        $this->settingColor = $settingColor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSettingDefaultAvatarStatus()
+    {
+        return $this->settingDefaultAvatarStatus;
+    }
+
+    /**
+     * @param mixed $settingDefaultAvatarStatus
+     */
+    public function setSettingDefaultAvatarStatus($settingDefaultAvatarStatus)
+    {
+        $this->settingDefaultAvatarStatus = $settingDefaultAvatarStatus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSettingRestrictionChat()
+    {
+        return $this->settingRestrictionChat;
+    }
+
+    /**
+     * @param mixed $settingRestrictionChat
+     */
+    public function setSettingRestrictionChat($settingRestrictionChat)
+    {
+        $this->settingRestrictionChat = $settingRestrictionChat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonetaryAccountBank()
+    {
+        return $this->monetaryAccountBank;
+    }
+
+    /**
+     * @param mixed $monetaryAccountBank
+     */
+    public function setMonetaryAccountBank($monetaryAccountBank)
+    {
+        $this->monetaryAccountBank = $monetaryAccountBank;
+    }
 }
