f484b4c9684e6569f5db806c1851a88e3f3c0c52
JvanHengel <jellevhengel@gmail.com>
2017-05-11 11:31:01 +0200

    Added monetaryAccountBank class

---
+++ src/Bunq/MonetaryAccounts/MonetaryAccountBank.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bunq\MonetaryAccounts;
+
+include_once('BunqObject.php');
+include_once('Client/BunqResponse.php');
+
+use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
+
+class MonetaryAccountBank extends BunqObject
+{
+
+}
