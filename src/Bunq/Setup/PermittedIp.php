57ae23c916515f3f22020783e43acc0c18952061
JvanHengel <jellevhengel@gmail.com>
2017-05-15 15:14:48 +0200

    Added PermittedIp object

---
+++ src/Bunq/Setup/PermittedIp.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Bunq\Setup;
+
+include_once('BunqObject.php');
+include_once('Client/BunqResponse.php');
+
+use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
+
+class PermittedIp extends BunqObject
+{
+    /**
+     * Request attributes:
+     */
+    private $ip;
+    private $status;
+
+    /**
+     * Response attributes:
+     */
+    private $response;
+
+    /**
+     * Extracts the response data and stores them in the class fields.
+     * @param BunqResponse $response the response returned by the server.
+     * @param $method String the http method used to get the response.
+     */
+    public function serializeData(BunqResponse $response, $method)
+    {
+        //Since the responses for the POST, PUT and GET methods are the same, $method can be ignored.
+        $this->response = json_decode($response->getBodyString())->{'Response'}[0];
+    }
+
+    /**
+     * @return array the request body as an array.
+     */
+    public function getRequestBodyArray()
+    {
+        $requestBody = [];
+
+        if(!is_null($this->ip)) {
+            $requestBody['ip'] = $this->ip;
+        }
+
+        if(!is_null($this->status)) {
+            $requestBody['status'] = $this->status;
+        }
+
+        return $requestBody;
+    }
+}
