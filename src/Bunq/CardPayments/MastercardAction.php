7935fabbcd007bb00b42a2202da60bdcc9c70642
JvanHengel <jellevhengel@gmail.com>
2017-05-15 19:23:38 +0200

    Added MastercardAction object

---
+++ src/Bunq/CardPayments/MastercardAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bunq\CardPayments;
+
+include_once('BunqObject.php');
+include_once('Client/BunqResponse.php');
+
+use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
+
+class MastercardAction extends BunqObject
+{
+
+}
