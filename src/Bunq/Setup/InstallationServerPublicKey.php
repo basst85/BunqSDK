b183594be32ae56b4c4b28f908f4421f76c6be56
JvanHengel <jellevhengel@gmail.com>
2017-05-15 14:51:45 +0200

    Added InstallationServerPublicKey object

---
+++ src/Bunq/Setup/InstallationServerPublicKey.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bunq\Setup;
+
+include_once('BunqObject.php');
+include_once('Client/BunqResponse.php');
+include_once('Exceptions/BunqObjectException.php');
+
+use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
+use Bunq\Exceptions\BunqObjectException;
+
+class InstallationServerPublicKey extends BunqObject
+{
+    /**
+     * Response attributes:
+     */
+    private $serverPublicKey;
+
+    /**
+     * Extracts the response data and stores them in the class fields.
+     * @param BunqResponse $response the response returned by the server.
+     * @param $method String the http method used to get the response.
+     */
+    public function serializeData(BunqResponse $response, $method)
+    {
+
+        if($method === 'GET') {
+            $this->serverPublicKey = json_decode($response->getBodyString())->{'Response'}[0]->{'ServerPublicKey'};
+        }
+    }
+}
