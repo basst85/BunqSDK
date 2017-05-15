eaf04e0f6b8d3b60cb7820b6b102634bd0ca915e
JvanHengel <jellevhengel@gmail.com>
2017-05-15 15:28:02 +0200

    Added Invoice object

---
+++ src/Bunq/Invoices/Invoice.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bunq\Invoices;
+
+include_once('BunqObject.php');
+include_once('Client/BunqResponse.php');
+
+use Bunq\BunqObject;
+use Bunq\Client\BunqResponse;
+
+class Invoice extends BunqObject
+{
+    /**
+     * Response attributes:
+     */
+    private $invoice;
+
+    /**
+     * Extracts the response data and stores them in the class fields.
+     * @param BunqResponse $response the response returned by the server.
+     * @param $method String the http method used to get the response.
+     */
+    public function serializeData(BunqResponse $response, $method)
+    {
+        if($method === 'GET') {
+            $this->invoice = json_decode($response->getBodyString())->{'Response'}[0]->{'Invoice'};
+        }
+    }
+}
