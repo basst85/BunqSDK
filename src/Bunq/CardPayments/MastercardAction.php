fbefbfac44f28e6619052d46ba87f63fdebdeba9
JvanHengel <jellevhengel@gmail.com>
2017-05-17 17:00:54 +0200

    Added response attributes

--- src/Bunq/CardPayments/MastercardAction.php
+++ src/Bunq/CardPayments/MastercardAction.php
@@ -9,5 +9,32 @@ use Bunq\Client\BunqResponse;
 
 class MastercardAction extends BunqObject
 {
+    /**
+     * Request attributes:
+     */
 
+    /**
+     * Response attributes:
+     */
+    private $monetaryAccountId;
+    private $cardId;
+    private $amountLocal;
+    private $amountBilling;
+    private $amountOriginalLocal;
+    private $amountOriginalBilling;
+    private $amountFee;
+    private $decision;
+    private $decisionDescription;
+    private $decisionDescriptionTranslated;
+    private $description;
+    private $authorisationStatus;
+    private $authorisationType;
+    private $city;
+    private $alias;
+    private $counterpartyAlias;
+    private $labelCard;
+    private $tokenStatus;
+    private $reservationExpiryTime;
+    private $appliedLimit;
+    private $allowChat;
 }
