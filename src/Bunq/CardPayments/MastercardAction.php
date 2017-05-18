e2530ded52cd0eacd931a7bd6c449f81341e3d20
JvanHengel <jellevhengel@gmail.com>
2017-05-18 12:59:18 +0200

    Updated serialzeData method with correct pointers

--- src/Bunq/CardPayments/MastercardAction.php
+++ src/Bunq/CardPayments/MastercardAction.php
@@ -42,26 +42,26 @@ class MastercardAction extends BunqObject
     {
         if($method === 'GET') {
             $this->monetaryAccountId = json_decode($response->getBodyString())->{'Response'}[0]->{'monetary_account_id'};
-            $this->cardId = json_decode($response->getBodyString())->{'Response'}[0]->{'card_id'};
-            $this->amountLocal = json_decode($response->getBodyString())->{'Response'}[0]->{'amount_local'};
-            $this->amountBilling = json_decode($response->getBodyString())->{'Response'}[0]->{'amount_billing'};
-            $this->amountOriginalLocal = json_decode($response->getBodyString())->{'Response'}[0]->{'amount_original_local'};
-            $this->amountOriginalBilling = json_decode($response->getBodyString())->{'Response'}[0]->{'amount_original_billing'};
-            $this->amountFee = json_decode($response->getBodyString())->{'Response'}[0]->{'amount_fee'};
-            $this->decision = json_decode($response->getBodyString())->{'Response'}[0]->{'decision'};
-            $this->decisionDescription = json_decode($response->getBodyString())->{'Response'}[0]->{'decision_description'};
-            $this->decisionDescriptionTranslated = json_decode($response->getBodyString())->{'Response'}[0]->{'decision_description_translated'};
-            $this->description = json_decode($response->getBodyString())->{'Response'}[0]->{'description'};
-            $this->authorisationStatus = json_decode($response->getBodyString())->{'Response'}[0]->{'authorisation_status'};
-            $this->authorisationType = json_decode($response->getBodyString())->{'Response'}[0]->{'authorisation_type'};
-            $this->city = json_decode($response->getBodyString())->{'Response'}[0]->{'city'};
-            $this->alias = json_decode($response->getBodyString())->{'Response'}[0]->{'alias'};
-            $this->counterPartyAlias = json_decode($response->getBodyString())->{'Response'}[0]->{'counterparty_alias'};
-            $this->labelCard = json_decode($response->getBodyString())->{'Response'}[0]->{'label_card'};
-            $this->tokenStatus = json_decode($response->getBodyString())->{'Response'}[0]->{'token_status'};
-            $this->reservationExpiryTime = json_decode($response->getBodyString())->{'Response'}[0]->{'reservation_expiry_time'};
-            $this->appliedLimit = json_decode($response->getBodyString())->{'Response'}[0]->{'applied_limit'};
-            $this->allowChat = json_decode($response->getBodyString())->{'Response'}[0]->{'allow_chat'};
+            $this->cardId = json_decode($response->getBodyString())->{'Response'}[1]->{'card_id'};
+            $this->amountLocal = json_decode($response->getBodyString())->{'Response'}[2]->{'amount_local'};
+            $this->amountBilling = json_decode($response->getBodyString())->{'Response'}[3]->{'amount_billing'};
+            $this->amountOriginalLocal = json_decode($response->getBodyString())->{'Response'}[4]->{'amount_original_local'};
+            $this->amountOriginalBilling = json_decode($response->getBodyString())->{'Response'}[5]->{'amount_original_billing'};
+            $this->amountFee = json_decode($response->getBodyString())->{'Response'}[6]->{'amount_fee'};
+            $this->decision = json_decode($response->getBodyString())->{'Response'}[7]->{'decision'};
+            $this->decisionDescription = json_decode($response->getBodyString())->{'Response'}[8]->{'decision_description'};
+            $this->decisionDescriptionTranslated = json_decode($response->getBodyString())->{'Response'}[9]->{'decision_description_translated'};
+            $this->description = json_decode($response->getBodyString())->{'Response'}[10]->{'description'};
+            $this->authorisationStatus = json_decode($response->getBodyString())->{'Response'}[11]->{'authorisation_status'};
+            $this->authorisationType = json_decode($response->getBodyString())->{'Response'}[12]->{'authorisation_type'};
+            $this->city = json_decode($response->getBodyString())->{'Response'}[13]->{'city'};
+            $this->alias = json_decode($response->getBodyString())->{'Response'}[14]->{'alias'};
+            $this->counterPartyAlias = json_decode($response->getBodyString())->{'Response'}[15]->{'counterparty_alias'};
+            $this->labelCard = json_decode($response->getBodyString())->{'Response'}[16]->{'label_card'};
+            $this->tokenStatus = json_decode($response->getBodyString())->{'Response'}[17]->{'token_status'};
+            $this->reservationExpiryTime = json_decode($response->getBodyString())->{'Response'}[18]->{'reservation_expiry_time'};
+            $this->appliedLimit = json_decode($response->getBodyString())->{'Response'}[19]->{'applied_limit'};
+            $this->allowChat = json_decode($response->getBodyString())->{'Response'}[20]->{'allow_chat'};
         }
     }
 
